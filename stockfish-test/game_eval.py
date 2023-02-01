ba0333f002a54bca6fe7178d337b0d1e334baad0
tom-newbold <thomas_newbold@outlook.com>
2023-02-01 12:04:38 +0000

    applys moves with python-chess

--- stockfish-test/game_eval.py
+++ stockfish-test/game_eval.py
@@ -9,8 +9,28 @@ player = 0 if '"The11thTom"' in out[4] else 1
 moves = ''
 for k in range(13,len(out)):
     moves = moves + ' ' + out[k]
-print(moves)
+
+_moves = moves.split(' ')[1:]
+m = []
+for i in range(len(_moves)//3):
+    m.append(_moves[i*3+1])
+    m.append(_moves[i*3+2])
+print(m)
+
 
 from stockfish import Stockfish as sf
 
 stockfish_instance = sf('stockfish_20011801_x64.exe')
+
+import chess.pgn
+from io import StringIO as sIO
+game = chess.pgn.read_game(sIO(moves))
+b = game.board()
+for m_i in range(len(m)-1):
+    move = b.push_san(m[m_i]) #get lan
+    #print(move)
+fen = b.fen()
+print(fen)
+print(b.outcome())
+stockfish_instance.set_fen_position(fen)
+print(stockfish_instance.get_board_visual())
