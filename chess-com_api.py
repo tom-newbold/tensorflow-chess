8f508d5fee3b0e1fbfeb17f2c472776dc7f9aac4
tom-newbold <thomas_newbold@outlook.com>
2023-02-02 12:45:00 +0000

    moved back to main folder (parent imports failed)

--- chess-com_api.py
+++ chess-com_api.py
@@ -5,7 +5,7 @@ m = time.gmtime().tm_mon
 # fetch most recent game
 r = []
 while len(r) == 0:
-    request = requests.get('https://api.chess.com/pub/player/benw2349/games/{year}/{month:02d}'.format(year=y,month=m))
+    request = requests.get('https://api.chess.com/pub/player/coolazero/games/{year}/{month:02d}'.format(year=y,month=m))
     r = request.json()['games']
     m -= 1
     if m==0:
@@ -17,7 +17,7 @@ file = open('game-from-api.pgn', 'w')
 file.write(j['pgn'])
 file.close()
 
-from . import coupled_stockfish_engine as cse
-sf = cse.CoupledStockfish('game-from-api.pgn', 'benw2349')
+import coupled_stockfish_engine as cse
+sf = cse.CoupledStockfish('game-from-api.pgn', 'CoolaZero')
 b = sf.run()
 print(sf.get_sf_board())
