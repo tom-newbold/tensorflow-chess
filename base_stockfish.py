1db9971506cd407b7deef713e3a1109ac675ff97
tom-newbold <thomas_newbold@outlook.com>
2023-02-03 10:09:45 +0000

    move validity from fen

---
+++ base_stockfish.py
@@ -0,0 +1,11 @@
+STOCKFISH_PATH = 'stockfish-test\\stockfish_20011801_x64.exe'
+
+from stockfish import Stockfish
+
+class SF: ## put into coupled stockfish (make_move func)
+    def __init__(self):
+        self.stockfish_instance = Stockfish(path=STOCKFISH_PATH)
+
+    def check_move(self, board_fen, move_lan):
+        self.stockfish_instance.set_fen_position(board_fen)
+        return self.stockfish_instance.is_move_correct(move_lan)
