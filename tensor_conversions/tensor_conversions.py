d483879935770af34d2d641591225fba60b58bd6
tom-newbold <thomas_newbold@outlook.com>
2023-02-10 13:52:00 +0000

    _

--- tensor_conversions/tensor_conversions.py
+++ tensor_conversions/tensor_conversions.py
@@ -24,11 +24,11 @@ def fen_to_tensor(fen_string: str) -> tf.Tensor:
 
 #print(fen_to_tensor("r1bq2nr/2pk1Bpp/p4p2/np2p3/1P3P2/PQP1P2P/6P1/R1B1K1NR w KQ - 3 14"))
 
-def lan_to_tensor(lan_string: str, player) -> tf.Tensor:
+def lan_to_tensor(lan_string: str, player: int) -> tf.Tensor:
     a = np.zeros((2, 8, 8))
     if 'O-O' in lan_string: # special case for castling; [e1h1-f1g1, e8h8-f8g8]
         rank = 1 if player==0 else 8
-        if lan_string == 'O-O-O': # queenside castle; [e1a1-c1d1, ]
+        if lan_string == 'O-O-O': # queenside castle; [e1a1-c1d1, e8a8-c8d8]
             a[0, 8-rank, 4] = 1
             a[0, 8-rank, 0] = 1
             a[1, 8-rank, 2] = 1
@@ -45,7 +45,6 @@ def lan_to_tensor(lan_string: str, player) -> tf.Tensor:
         elif 'x' in lan_string:
             lan_string = lan_string.split('x')
         s = [lan_string[0][-2:], lan_string[1]]
-        #castling is an issue
         for i in range(2):
             a[i, 8-int(s[i][1]), 'abcdefgh'.index(s[i][0])] = 1
     return tf.convert_to_tensor(a)
